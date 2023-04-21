7ea33c864306b93b67aa98c2eaffcacf7d34071a
George Orlov <110418328+Tanjero19@users.noreply.github.com>
2023-04-21 13:57:56 +0500

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const mobileButton = document.querySelector('.nav_button');
+const mobileNav = document.querySelector('.mobile_nav');
+
+mobileButton.addEventListener('click', function () {
+    mobileNav.classList.toggle('mobile_nav_active');
+})
