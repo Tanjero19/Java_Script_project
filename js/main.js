17812c39a0e72a483fcafa1de1376775cd98671d
George Orlov <110418328+Tanjero19@users.noreply.github.com>
2023-04-23 13:10:46 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,3 +4,8 @@ const mobileNav = document.querySelector('.mobile_nav');
 mobileButton.addEventListener('click', function () {
     mobileNav.classList.toggle('mobile_nav_active');
 })
+
+    function toggleClass(ementId, className) {
+        const element = document.getElementById(ementId);
+        element.classList.toggle(className);
+    }
