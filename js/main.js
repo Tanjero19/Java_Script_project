b25b9d4214279db6ebe91f118dab2ae081eb6702
George Orlov <110418328+Tanjero19@users.noreply.github.com>
2023-10-26 18:44:11 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 const mobileButton = document.querySelector('.nav_button');
 const mobileNav = document.querySelector('.mobile_nav');
 
-mobileButton.addEventListener('click', function () {
+
+    mobileButton.addEventListener('click', function () {
     mobileNav.classList.toggle('mobile_nav_active');
 })
 
-    function toggleClass(ementId, className) {
-        const element = document.getElementById(ementId);
-        element.classList.toggle(className);
-    }
+function toggleClass(elementId, className) {
+    const element = document.getElementById(elementId);
+    element.classList.toggle(className);
+}
+
